e44e510138f7a2d9d085a9692eb469731e94b5a3
Rishab Garg <mail@rishab-garg.de>
2026-06-30 19:11:16 +0200

    refactor(java): use own options instead of vars

--- home/rishab/langs/java.nix
+++ home/rishab/langs/java.nix
@@ -4,34 +4,44 @@
   lib,
   ...
 }:
-
+let
+  cfg = config.langs.java;
+in
 {
-  vars.jdks = with pkgs; [
-    jdk17
-    jdk21
-    jdk25
-  ];
-
-  home.activation =
-    lib.listToAttrs
-    <| lib.map (
-      jdk:
-      lib.nameValuePair "java-${lib.versions.major jdk.version}" (
-        lib.hm.dag.entryAfter [ "writeBoundary" ] (''
-          target_root="/Library/Java/JavaVirtualMachines"
-          run mkdir -p "$target_root"
-
-          run ln -sfn $VERBOSE_ARG \
-            "${jdk.outPath}/Library/Java/JavaVirtualMachines/zulu-${lib.versions.major jdk.version}.jdk" \
-          "$target_root/zulu-${lib.versions.major jdk.version}.jdk"
-        '')
-      )
-    )
-    <| config.vars.jdks;
-
-  home.sessionVariables = {
-    JAVA_HOME = "$(/usr/libexec/java_home)";
+  options.langs.java = {
+    jdks = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = with pkgs; [
+        jdk17
+        jdk21
+        jdk25
+      ];
+      description = "java packages to use";
+    };
   };
-  home.sessionPath = [ "${config.home.sessionVariables.JAVA_HOME}/bin" ];
 
+  config = {
+
+    home.activation =
+      lib.listToAttrs
+      <| lib.map (
+        jdk:
+        lib.nameValuePair "java-${lib.versions.major jdk.version}" (
+          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            target_root="$HOME/Library/Java/JavaVirtualMachines"
+            run mkdir -p "$target_root"
+
+            run ln -sfn $VERBOSE_ARG \
+              "${jdk.outPath}/Library/Java/JavaVirtualMachines/zulu-${lib.versions.major jdk.version}.jdk" \
+            "$target_root/zulu-${lib.versions.major jdk.version}.jdk"
+          ''
+        )
+      )
+      <| cfg.jdks;
+
+    home.sessionVariables = {
+      JAVA_HOME = "$(/usr/libexec/java_home)";
+    };
+    home.sessionPath = [ "${config.home.sessionVariables.JAVA_HOME}/bin" ];
+  };
 }
