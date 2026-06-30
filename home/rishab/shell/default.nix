fc30eebde0a30ab37a6f76f0733c36c9e606fee5
Rishab Garg <mail@rishab-garg.de>
2026-06-30 19:11:16 +0200

    chore(colours): move fish colours to shell.nix

--- home/rishab/shell/default.nix
+++ home/rishab/shell/default.nix
@@ -234,4 +234,38 @@ in
     starship-jj
   ];
 
+  xdg.configFile."fish/themes/stylix.theme".text = with config.lib.stylix.colors; ''
+    # Syntax Highlighting Colors
+    fish_color_normal ${base05}
+    fish_color_command ${base0E}
+    fish_color_quote ${base0B}
+    fish_color_redirection ${base0C}
+    fish_color_end ${base05}
+    fish_color_error ${base08}
+    fish_color_param ${base08}
+    fish_color_comment ${base04}
+    fish_color_match ${base0C} --underline
+    fish_color_search_match --background=2e6399
+    fish_color_operator ${base0E}
+    fish_color_escape ${base0C}
+    fish_color_cwd ${base08}
+    fish_color_autosuggestion ${base05}
+    fish_color_valid_path ${base08} --underline
+    fish_color_history_current ${base0C}
+    fish_color_selection --background=${base04}
+    fish_color_user ${base0D}
+    fish_color_host ${base0B}
+    fish_color_cancel ${base04}
+
+    # Completion Pager Colors
+    fish_pager_color_completion ${base05}
+    fish_pager_color_prefix ${base0B}
+    fish_pager_color_description ${base05}
+    fish_pager_color_progress ${base05}
+  '';
+
+  programs.fish.interactiveShellInit = ''
+    fish_config theme choose stylix
+  '';
+
 }
