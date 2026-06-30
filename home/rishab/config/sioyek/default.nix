8c8ddec16f51541c614c39001ca1352fa5c5dd88
Rishab Garg <mail@rishab-garg.de>
2026-06-30 19:11:16 +0200

    feat(sioyek): update config

--- home/rishab/config/sioyek/default.nix
+++ home/rishab/config/sioyek/default.nix
@@ -1,19 +1,18 @@
-{
-  config,
-  lib,
-  ...
-}:
-{
+{ ... }: {
   programs.sioyek = {
     enable = true;
     bindings = {
       "toggle_custom_color" = "i";
+      "goto_prev_tab" = "<Ctrl h>";
+      "goto_next_tab" = "<Ctrl l>";
     };
     config = {
       "ui_font" = "Maple Mono NF CN";
       "font_size" = "15";
 
       "fit_to_page_width_on_open" = "1";
+
+      "page_space_y" = "10";
     };
   };
 }
