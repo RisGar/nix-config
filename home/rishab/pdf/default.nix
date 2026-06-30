4c126883c2ae90d5cb2d8617f4f25cdc4585863f
Rishab Garg <mail@rishab-garg.de>
2026-06-30 19:11:16 +0200

    feat(pdf): add new sioyek config options

--- home/rishab/pdf/default.nix
+++ home/rishab/pdf/default.nix
@@ -5,14 +5,14 @@
       "toggle_custom_color" = "i";
       "goto_prev_tab" = "<Ctrl h>";
       "goto_next_tab" = "<Ctrl l>";
+
+      "toggle_freehand_drawing_mode" = "d";
     };
     config = {
       "ui_font" = "Maple Mono NF CN";
       "font_size" = "15";
-
       "fit_to_page_width_on_open" = "1";
-
-      "page_space_y" = "10";
+      "page_padding_y" = "10";
     };
   };
 }
